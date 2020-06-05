a17daee794712bb1941281df2631b75d55660276
Preetham.Armoor <preetham.armoor@gmail.com>
2020-06-05 21:59:41 +0200

    request and app routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "working in progress to serve NLP Machine learning models"
-
+'''
 @app.route('/text', methods=['GET', 'POST'])
 def text():
     if request.method == 'POST':
@@ -15,8 +15,12 @@ def text():
         show_the_login_form()
     return render_template('form.html',form=form)
 
-
+'''
+@app.route('/text')
+def text():
+    user_agent=request.headers.get("user-agent")
+    return "<p>Your browser is %s </p>" % user_agent
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
